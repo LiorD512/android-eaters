7bee25717da7cf6822a11a6891bf5caddf3c2648
amittelman <alon@monkeytech.co.il>
2021-07-13 18:36:47 +0300

    feed ui developmed

--- app/src/main/java/com/bupp/wood_spoon_eaters/views/feed_views/campaign_section/FeedCampaignSection.kt
+++ app/src/main/java/com/bupp/wood_spoon_eaters/views/feed_views/campaign_section/FeedCampaignSection.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.bumptech.glide.Glide
+import com.bupp.wood_spoon_eaters.databinding.FeedCampaignSectionBinding
 import com.bupp.wood_spoon_eaters.databinding.ShareBannerBinding
 import com.bupp.wood_spoon_eaters.model.Campaign
 
@@ -14,31 +15,31 @@ class FeedCampaignSection @JvmOverloads
 constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     ConstraintLayout(context, attrs, defStyleAttr) {
 
-    private var binding: ShareBannerBinding = ShareBannerBinding.inflate(LayoutInflater.from(context), this, true)
+    private var binding: FeedCampaignSectionBinding = FeedCampaignSectionBinding.inflate(LayoutInflater.from(context), this, true)
     private var curCampaign: Campaign? = null
-    var listener: WSCustomBannerListener? = null
-
-
-    interface WSCustomBannerListener {
-        fun onShareBannerClick(campaign: Campaign?){}
-    }
-
-    fun initCustomBanner(campaign: Campaign, listener: WSCustomBannerListener){
-        this.listener = listener
-        this.curCampaign = campaign
-        handleData(campaign)
-    }
-
-    private fun handleData(campaign: Campaign) {
-        Glide.with(context).load(campaign.photoLarge).into(binding.customBanner)
-        binding.customBannerTitle.text = campaign.header
-        binding.customBannerSubTitle.text = campaign.bodyText1
-
-        binding.customBannerLayout.setOnClickListener {
-            listener?.onShareBannerClick(curCampaign)
-        }
-
-        binding.customBannerLayout.visibility = View.VISIBLE
-    }
+//    var listener: WSCustomBannerListener? = null
+//
+//
+//    interface WSCustomBannerListener {
+//        fun onShareBannerClick(campaign: Campaign?){}
+//    }
+//
+//    fun initCustomBanner(campaign: Campaign, listener: WSCustomBannerListener){
+//        this.listener = listener
+//        this.curCampaign = campaign
+//        handleData(campaign)
+//    }
+//
+//    private fun handleData(campaign: Campaign) {
+//        Glide.with(context).load(campaign.photoLarge).into(binding.customBanner)
+//        binding.customBannerTitle.text = campaign.header
+//        binding.customBannerSubTitle.text = campaign.bodyText1
+//
+//        binding.customBannerLayout.setOnClickListener {
+//            listener?.onShareBannerClick(curCampaign)
+//        }
+//
+//        binding.customBannerLayout.visibility = View.VISIBLE
+//    }
 
 }
---
+++ app/src/main/java/com/bupp/wood_spoon_eaters/views/feed_views/restaurant_section/FeedRestaurantSection.kt
@@ -0,0 +1,30 @@
+package com.bupp.wood_spoon_eaters.views.feed_views.restaurant_section
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.Glide
+import com.bupp.wood_spoon_eaters.databinding.FeedCampaignSectionBinding
+import com.bupp.wood_spoon_eaters.databinding.FeedRestaurantSectionBinding
+import com.bupp.wood_spoon_eaters.databinding.ShareBannerBinding
+import com.bupp.wood_spoon_eaters.model.Campaign
+
+
+class FeedRestaurantSection @JvmOverloads
+constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+    ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private var binding: FeedRestaurantSectionBinding = FeedRestaurantSectionBinding.inflate(LayoutInflater.from(context), this, true)
+
+
+    init{
+        initUi()
+    }
+
+    private fun initUi() {
+
+    }
+
+}
