e6bf7750c9c1b84b4073cc909480dba871fa15b6
karinkrupetsky <karin@monkeytech.co.il>
2021-08-03 14:37:43 +0300

    reviews section

--- app/src/main/java/com/bupp/wood_spoon_eaters/features/reviews/fragments/ReviewDetailsFragment.kt
+++ app/src/main/java/com/bupp/wood_spoon_eaters/features/reviews/fragments/ReviewDetailsFragment.kt
@@ -1,6 +1,8 @@
 package com.bupp.wood_spoon_eaters.features.reviews.fragments
 
 import android.os.Bundle
+import android.transition.Transition
+import android.transition.TransitionInflater
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -12,16 +14,86 @@ import com.bupp.wood_spoon_eaters.di.abs.LiveEvent
 import com.bupp.wood_spoon_eaters.features.reviews.ReviewsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+
 class ReviewDetailsFragment : Fragment(R.layout.fragment_review_details){
     val binding: FragmentReviewDetailsBinding by viewBinding()
     private val viewModel by viewModel<ReviewsViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val animation = TransitionInflater.from(requireContext()).inflateTransition(
+            android.R.transition.move
+        )
+        sharedElementEnterTransition = animation
+        sharedElementReturnTransition = animation
+
+        animation.addListener(object  : Transition.TransitionListener{
+            override fun onTransitionStart(transition: Transition?) {
+            }
+
+            override fun onTransitionEnd(transition: Transition?) {
+                fadeIn()
+            }
+
+            override fun onTransitionCancel(transition: Transition?) {
+            }
+
+            override fun onTransitionPause(transition: Transition?) {
+            }
+
+            override fun onTransitionResume(transition: Transition?) {
+            }
+
+        })
+
+
+    }
+
+//    fun fadeIn()
+//    {
+//        val animation = AnimationUtils.loadAnimation(context , R.anim.fade_in)
+//        binding.editTextReviewDetails.animation = animation
+//    }
+
+    private fun  fadeIn() {
+        binding.editTextLayoutReview?.let {
+            binding.editTextLayoutReview.apply {
+                // Set the content view to 0% opacity but visible, so that it is visible
+                // (but fully transparent) during the animation.
+                alpha = 0f
+                visibility = View.VISIBLE
+
+                // Animate the content view to 100% opacity, and clear any animation
+                // listener set on the view.
+                animate()
+                    .alpha(1f)
+                    .setDuration(600)
+
+            }
+        }
+
+        binding.reviewExperienceFragmentSubmitBtn?.let {
+            binding.reviewExperienceFragmentSubmitBtn.apply {
+                // Set the content view to 0% opacity but visible, so that it is visible
+                // (but fully transparent) during the animation.
+                alpha = 0f
+                visibility = View.VISIBLE
+
+                // Animate the content view to 100% opacity, and clear any animation
+                // listener set on the view.
+                animate()
+                    .alpha(1f)
+                    .setDuration(600)
+
+            }
+        }
+
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initObservers()
-//        initUi()
     }
 
     private fun initObservers() {
--- app/src/main/java/com/bupp/wood_spoon_eaters/features/reviews/fragments/ReviewExperienceFragment.kt
+++ app/src/main/java/com/bupp/wood_spoon_eaters/features/reviews/fragments/ReviewExperienceFragment.kt
@@ -3,17 +3,19 @@ package com.bupp.wood_spoon_eaters.features.reviews.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bupp.wood_spoon_eaters.R
+import com.bupp.wood_spoon_eaters.databinding.ActivityReviewBinding
 import com.bupp.wood_spoon_eaters.databinding.FragmentReviewExperienceBinding
 import com.bupp.wood_spoon_eaters.di.abs.LiveEvent
 import com.bupp.wood_spoon_eaters.features.reviews.ReviewsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ReviewExperienceFragment() : Fragment(R.layout.fragment_review_experience) {
+
+
 
     val binding: FragmentReviewExperienceBinding by viewBinding()
     private val viewModel by viewModel<ReviewsViewModel>()
@@ -30,28 +32,26 @@ class ReviewExperienceFragment() : Fragment(R.layout.fragment_review_experience)
         binding.reviewExperienceFragmentNextBtn.setOnClickListener {
             viewModel.onNextClick()
         }
-
     }
+
     private fun initObservers() {
-        viewModel.navigationEvent.observe(viewLifecycleOwner, Observer { navigationEvent ->
+        viewModel.navigationEvent.observe(viewLifecycleOwner,  { navigationEvent ->
             handleNavigationEvent(navigationEvent)
         })
     }
 
     private fun handleNavigationEvent(navigationEvent: LiveEvent<ReviewsViewModel.NavigationEvent>?) {
         navigationEvent?.getContentIfNotHandled()?.let {
+            val extras = FragmentNavigatorExtras(binding.image to "logo_transition", binding.title to "title_transition", binding.subtitle to "subtitle_transition")
+
             when (it) {
                 ReviewsViewModel.NavigationEvent.EXPERIENCE_TO_DETAILS -> {
                     val action = ReviewExperienceFragmentDirections.actionReviewExperienceFragmentToReviewDetailsFragment()
-                    findNavController().navigate(action)
+                    findNavController().navigate(action,extras)
                 }
             }
         }
     }
-
-
-
-
 
 
 
