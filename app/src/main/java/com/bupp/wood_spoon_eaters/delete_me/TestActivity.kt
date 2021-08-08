921bf72231833810870286fc978ae7e77828a071
karinkrupetsky <karin@monkeytech.co.il>
2021-08-08 11:44:43 +0300

    reviews list

--- app/src/main/java/com/bupp/wood_spoon_eaters/delete_me/TestActivity.kt
+++ app/src/main/java/com/bupp/wood_spoon_eaters/delete_me/TestActivity.kt
@@ -2,26 +2,31 @@ package com.bupp.wood_spoon_eaters.delete_me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import com.bupp.wood_spoon_eaters.databinding.ActivityTestBinding
 import com.bupp.wood_spoon_eaters.features.new_order.sub_screen.cart_bottom_sheet.CartBottomSheet
 import com.bupp.wood_spoon_eaters.features.new_order.sub_screen.upsale_cart_bottom_sheet.UpSaleNCartBottomSheet
+import org.w3c.dom.Text
 
-class TestActivity : AppCompatActivity() {
+class TestActivity : ComponentActivity() {
 
     lateinit var binding: ActivityTestBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityTestBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        UpSaleNCartBottomSheet().show(supportFragmentManager, "upsale")
-
-
-        binding.btn.setOnClickListener {
-            UpSaleNCartBottomSheet().show(supportFragmentManager, "upsale")
-//            CartBottomSheet().show(supportFragmentManager, "upsale2")
-        }
+//        setContent{
+//            Text("hello world")
+//        }
+//        binding = ActivityTestBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        UpSaleNCartBottomSheet().show(supportFragmentManager, "upsale")
+//
+//
+//        binding.btn.setOnClickListener {
+//            UpSaleNCartBottomSheet().show(supportFragmentManager, "upsale")
+////            CartBottomSheet().show(supportFragmentManager, "upsale2")
+//        }
 
     }
 }
