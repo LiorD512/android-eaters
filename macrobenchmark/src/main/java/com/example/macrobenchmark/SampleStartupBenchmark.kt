f552488c838eee23e020c4111020129a6304f50f
unknown <alon@monkeytech.co.il>
2021-09-02 15:07:26 +0300

    benchmark semi completed

--- macrobenchmark/src/main/java/com/example/macrobenchmark/SampleStartupBenchmark.kt
+++ macrobenchmark/src/main/java/com/example/macrobenchmark/SampleStartupBenchmark.kt
@@ -1,35 +1,35 @@
-package com.example.macrobenchmark
-
-import android.content.Intent
-import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.StartupTimingMetric
-import androidx.benchmark.macro.junit4.MacrobenchmarkRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-
-@RunWith(AndroidJUnit4::class)
-class SampleStartupBenchmark {
-
-    @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
-
-    @Test
-    fun startup() = benchmarkRule.measureRepeated(
-        packageName = TARGET_PACKAGE,
-        metrics = listOf(StartupTimingMetric()),
-        iterations = 5,
-        startupMode = StartupMode.COLD
-    ) {
-        pressHome()
-        val intent = Intent()
-        intent.setPackage(TARGET_PACKAGE)
-        startActivityAndWait(intent)
-    }
-
-    companion object{
-        const val TARGET_PACKAGE = "com.bupp.wood_spoon_eaters"
-    }
-
-}
+//package com.example.macrobenchmark
+//
+//import android.content.Intent
+//import androidx.benchmark.macro.StartupMode
+//import androidx.benchmark.macro.StartupTimingMetric
+//import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+//import androidx.test.ext.junit.runners.AndroidJUnit4
+//import org.junit.Rule
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//
+//@RunWith(AndroidJUnit4::class)
+//class SampleStartupBenchmark {
+//
+//    @get:Rule
+//    val benchmarkRule = MacrobenchmarkRule()
+//
+//    @Test
+//    fun startup() = benchmarkRule.measureRepeated(
+//        packageName = TARGET_PACKAGE,
+//        metrics = listOf(StartupTimingMetric()),
+//        iterations = 5,
+//        startupMode = StartupMode.COLD
+//    ) {
+//        pressHome()
+//        val intent = Intent()
+//        intent.setPackage(TARGET_PACKAGE)
+//        startActivityAndWait(intent)
+//    }
+//
+//    companion object{
+//        const val TARGET_PACKAGE = "com.bupp.wood_spoon_eaters"
+//    }
+//
+//}
--- macrobenchmark/src/main/java/com/example/macrobenchmark/SplashStartupBenchmark.kt
+++ macrobenchmark/src/main/java/com/example/macrobenchmark/SplashStartupBenchmark.kt
@@ -1,37 +1,37 @@
-package com.example.macrobenchmark
-
-
-import android.content.Intent
-import androidx.benchmark.macro.StartupMode
-import androidx.benchmark.macro.junit4.MacrobenchmarkRule
-import androidx.test.filters.LargeTest
-import org.junit.Rule
-import org.junit.Test
-import org.junit.runner.RunWith
-import org.junit.runners.Parameterized
-
-@LargeTest
-@RunWith(Parameterized::class)
-class SplashStartupBenchmark(private val startupMode: StartupMode) {
-    @get:Rule
-    val benchmarkRule = MacrobenchmarkRule()
-
-    @Test
-    fun startup() = benchmarkRule.measureStartup(
-        profileCompiled = false,
-        startupMode = startupMode,
-        iterations = 3
-    ) {
-        action = "com.bupp.wood_spoon_eaters.features.splash.TRIVIAL_STARTUP_ACTIVITY"
-    }
-
-    companion object {
-        @Parameterized.Parameters(name = "mode={0}")
-        @JvmStatic
-        fun parameters(): List<Array<Any>> {
-            return listOf(StartupMode.COLD, StartupMode.WARM, StartupMode.HOT)
-                .map { arrayOf(it) }
-        }
-    }
-}
-
+//package com.example.macrobenchmark
+//
+//
+//import android.content.Intent
+//import androidx.benchmark.macro.StartupMode
+//import androidx.benchmark.macro.junit4.MacrobenchmarkRule
+//import androidx.test.filters.LargeTest
+//import org.junit.Rule
+//import org.junit.Test
+//import org.junit.runner.RunWith
+//import org.junit.runners.Parameterized
+//
+//@LargeTest
+//@RunWith(Parameterized::class)
+//class SplashStartupBenchmark(private val startupMode: StartupMode) {
+//    @get:Rule
+//    val benchmarkRule = MacrobenchmarkRule()
+//
+//    @Test
+//    fun startup() = benchmarkRule.measureStartup(
+//        profileCompiled = false,
+//        startupMode = startupMode,
+//        iterations = 3
+//    ) {
+//        action = "com.bupp.wood_spoon_eaters.features.splash.TRIVIAL_STARTUP_ACTIVITY"
+//    }
+//
+//    companion object {
+//        @Parameterized.Parameters(name = "mode={0}")
+//        @JvmStatic
+//        fun parameters(): List<Array<Any>> {
+//            return listOf(StartupMode.COLD, StartupMode.WARM, StartupMode.HOT)
+//                .map { arrayOf(it) }
+//        }
+//    }
+//}
+//
