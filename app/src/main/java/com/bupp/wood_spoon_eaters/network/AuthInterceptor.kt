cab8a7608dcbd1005d152ddf84ba78b5f3b24513
aMittelman <alon@monkeytech.co.il>
2021-10-24 19:37:07 +0300

    fix

--- app/src/main/java/com/bupp/wood_spoon_eaters/network/AuthInterceptor.kt
+++ app/src/main/java/com/bupp/wood_spoon_eaters/network/AuthInterceptor.kt
@@ -1,10 +1,12 @@
 package com.bupp.wood_spoon_eaters.network
 
 import android.util.Log
+import com.bupp.wood_spoon_eaters.common.FlavorConfigManager
 //import com.bupp.wood_spoon_eaters.model.LoginResponse
 import okhttp3.Interceptor
 import okhttp3.Request
 import okhttp3.Response
+import retrofit2.Invocation
 import java.io.IOException
 import java.util.concurrent.locks.ReentrantReadWriteLock
 
@@ -17,7 +19,7 @@ class AuthInterceptor(private val settings: ApiSettings) : Interceptor {
 
     @Throws(IOException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
-        var authToken: String?
+        val authToken: String?
 
         var request = chain.request()
         lock.readLock().lock()
@@ -27,8 +29,9 @@ class AuthInterceptor(private val settings: ApiSettings) : Interceptor {
             lock.readLock().unlock()
         }
 
+        request = buildVersionPath(request)
         request = addTokenToRequest(request, authToken)
-        var response = chain.proceed(request)
+        val response = chain.proceed(request)
 
         if (!response.header("X-Auth-Token").isNullOrEmpty()) {
             val newToken = response.header("X-Auth-Token") as String
@@ -57,6 +60,30 @@ class AuthInterceptor(private val settings: ApiSettings) : Interceptor {
             val requestBuilder = request.newBuilder().addHeader("X-Auth-Token", authToken)
             finalRequest = requestBuilder.build()
         }
+        return finalRequest
+    }
+
+    private fun buildVersionPath(request: Request): Request {
+        val invocation = request.tag(Invocation::class.java)
+        val version = invocation?.method()?.getAnnotation(V3::class.java)
+
+        val host = request.url.toUrl().host
+        var path = request.url.toUrl().path
+
+        if (version != null) {
+            //Annotation is present, using new auth method
+            path = path.replace(FlavorConfigManager.VERSION_PLACE_HOLDER, "v3")
+            Log.d(TAG, "version: $version - v3 - $path")
+        } else {
+            //No annotation, sticking with legacy auth
+            Log.d(TAG, "version: $version - v2")
+            path = path.replace(FlavorConfigManager.VERSION_PLACE_HOLDER, "v2")
+        }
+
+        val finalUrl = "https://$host$path"
+        Log.d(TAG, "finalUrl: $finalUrl")
+
+        val finalRequest = request.newBuilder().url(finalUrl).build()
         return finalRequest
     }
 
