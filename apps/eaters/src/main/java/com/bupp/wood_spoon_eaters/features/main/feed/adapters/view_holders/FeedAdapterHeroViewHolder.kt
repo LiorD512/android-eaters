e358c9aa48a354f889eae00e06b151191456cb46
Philipp Buhaievskyi <philippdevel@gmail.com>
2022-06-23 15:02:03 +0300

    MB-402: loop viewPager in hero section (#101)
    Co-authored-by: 😎pylypbuhaievskyi <philipp@eatwoodspoon.com>

--- apps/eaters/src/main/java/com/bupp/wood_spoon_eaters/features/main/feed/adapters/view_holders/FeedAdapterHeroViewHolder.kt
+++ apps/eaters/src/main/java/com/bupp/wood_spoon_eaters/features/main/feed/adapters/view_holders/FeedAdapterHeroViewHolder.kt
@@ -2,23 +2,51 @@ package com.bupp.wood_spoon_eaters.features.main.feed.adapters.view_holders
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.bupp.wood_spoon_eaters.databinding.FeedAdapterHeroSectionBinding
-import com.bupp.wood_spoon_eaters.features.main.feed.adapters.decorators.FeedCampaignCouponItemTransformer
 import com.bupp.wood_spoon_eaters.model.FeedAdapterHero
 
 class FeedAdapterHeroViewHolder(
     val binding: FeedAdapterHeroSectionBinding
 ) : RecyclerView.ViewHolder(binding.root) {
 
+    private lateinit var pagerCallback: ViewPager2.OnPageChangeCallback
+
     fun bindItems(
         item: FeedAdapterHero,
         listener: FeedHeroSectionPagerAdapter.FeedHeroSectionListener
     ) {
         val adapter = FeedHeroSectionPagerAdapter(listener)
+        val endlessList =
+            listOf(item.heroList.last()) + item.heroList + listOf(item.heroList.first())
+        adapter.submitList(endlessList)
 
-        binding.feedHeroSectionViewPager.adapter = adapter
-        binding.feedHeroSectionViewPager.setPageTransformer(FeedCampaignCouponItemTransformer())
-        adapter.submitList(item.heroList)
+        binding.apply {
+            feedHeroSectionViewPager.adapter = adapter
+            feedHeroSectionViewPager.setCurrentItem(1, false)
+
+            pagerCallback = object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageScrollStateChanged(state: Int) {
+                    when (state) {
+                        ViewPager2.SCROLL_STATE_IDLE -> {
+                            if (state == ViewPager2.SCROLL_STATE_IDLE) {
+                                when (binding.feedHeroSectionViewPager.currentItem) {
+                                    endlessList.lastIndex ->
+                                        binding.feedHeroSectionViewPager.setCurrentItem(1, false)
+                                    0 ->
+                                        binding.feedHeroSectionViewPager.setCurrentItem(
+                                            endlessList.size - 2,
+                                            false
+                                        )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            feedHeroSectionViewPager.registerOnPageChangeCallback(pagerCallback)
+        }
 
         if (item.heroList.size > 1) {
             binding.feedHeroSectionIndicator.visibility = View.VISIBLE
