3d084f92a5a1265d71d87c0346f6174b7316c72e
alsidor <al.sidorenko@gmail.com>
2022-11-15 12:11:39 +0000

    Hotfix/lint issue fix (#275)
    * Hard disbled logger
    
    * lint bug causing linter crash

--- apps/cooks/src/main/java/com/bupp/wood_spoon_chef/analytics/event/AnalyticsEvent.kt
+++ apps/cooks/src/main/java/com/bupp/wood_spoon_chef/analytics/event/AnalyticsEvent.kt
@@ -1,9 +1,9 @@
 package com.bupp.wood_spoon_chef.analytics.event
 
-import com.bupp.wood_spoon_chef.analytics.TrackedArea
-import com.bupp.wood_spoon_chef.analytics.TrackedEvent
-
+//import com.bupp.wood_spoon_chef.analytics.TrackedArea
+//import com.bupp.wood_spoon_chef.analytics.TrackedEvent
+// Commented out due to lint bug causing linter crash
 interface AnalyticsEvent {
-    @TrackedArea val trackedArea: String
-    @TrackedEvent val trackedEvent: String
+    /*@TrackedArea*/ val trackedArea: String
+    /*@TrackedEvent*/ val trackedEvent: String
 }
